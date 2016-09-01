6b40c96fc1f0d6e6b15cf9067a874a1dd59c9356
Paul Talaga <talagap@uindy.edu>
2016-09-01 10:33:00 -0400

    Hello World w/ input

---
+++ ClassCode/W1-HelloWorld/helloWorld.cpp
@@ -0,0 +1,14 @@
+/* Name: Paul Talaga
+   Date: 9/1/16
+   Desc: Hello World program!
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout << "Hello World" << endl;
+
+    return 0;
+}
---
+++ ClassCode/W1-HelloWorld/helloWorld2.cpp
@@ -0,0 +1,17 @@
+/* Name: Paul Talaga
+   Date: 9/1/16
+   Desc: Hello World program with input!
+*/
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+  string something;
+  cout << "Tell me something:" << endl;
+  cin >> something;
+  cout << something << "? Really?  That's boring." << endl;
+  
+  return 0;
+}
