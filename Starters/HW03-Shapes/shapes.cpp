c768667a315d7eae28b9e24841018b8b2a7ddda0
Paul Talaga <talagap@uindy.edu>
2016-09-18 11:47:35 -0400

    Shapes starter

---
+++ Starters/HW03-Shapes/shapes.cpp
@@ -0,0 +1,31 @@
+/* Name: fill me in
+   Date: fill me in
+   HW 3: Picture Maker
+   Desc: Draws various shapes on the screen
+*/
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main(){
+
+  cout << "Welcome to Shape-Maker!\n";
+  
+  cout << " 1:Make a horizontal line\n";
+  cout << " 2:Make a number of SOS calls\n";
+  cout << " 3:Make a filled-in square of '*'s\n";
+  cout << " 4:Make a filled-in rectangle of '*'s\n";
+  cout << " 5:Make a framed rectangle with '*'s on the outside and empty inside.\n";
+  cout << " 6:Make a framed rectangle with lines.\n";
+  cout << " 7:Make a half-filled-in rectangle.\n";
+  cout << " 8:Make a right-triangle w/ lower left filled in\n";
+  cout << " 9:Make a right-triangle w/ upper left filled in\n";
+  cout << "10:Make a 10x10 multiplcation table.\n";
+
+  cout << "Which shape should I draw?";
+  
+  // TODO: Fill in your code here!
+  
+  return 0;
+}
