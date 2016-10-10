9844326d163d7795d5958eca0045006b1b10a2bf
Paul Talaga <talagap@uindy.edu>
2016-10-10 09:50:18 -0400

    Practice exam sum_from_to

---
+++ ClassCode/W7-Review-Recursion/flipParameters.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int sum_from_to(int low, int high){
+  if(low > high){
+    // flip high and low
+    int temp = low;
+    low = high;
+    high = temp;
+  }
+  // Low must be <= high
+  int sum = 0;
+  for(int i = low; i <= high; i++){
+    sum  = sum + i;
+    //sum += i;   // Or this
+  }
+  return sum;
+}
+
+int main(){
+  cout << sum_from_to(-5,0) << endl;
+  cout << sum_from_to(0,-5) << endl;
+  
+}
