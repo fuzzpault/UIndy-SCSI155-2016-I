1cc6b578e41c0e397876fc1228de4fa504c613a4
Paul Talaga <talagap@uindy.edu>
2016-10-11 09:07:35 -0400

    no default

---
+++ ClassCode/W7-Review-Recursion/noDefault.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+  int a,b,c,d,e;
+  cout << a << endl;
+  cout << b << endl;
+  cout << c << endl;
+  cout << d << endl;
+  cout << e << endl;
+  b = 100;
+  e = 100;
+}
