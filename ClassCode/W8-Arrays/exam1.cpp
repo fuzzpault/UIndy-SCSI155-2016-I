5c4ae46c94feb828b6bb2b60bfbe216a335816ae
Paul Talaga <talagap@uindy.edu>
2016-10-17 16:03:09 -0400

    HW6 Starter

---
+++ ClassCode/W8-Arrays/exam1.cpp
@@ -0,0 +1,51 @@
+/*
+Name: <fill me in>
+Date: <fill me in>
+Assignment: <fill me in>
+Description: Implementation of programming problems on the Midterm Exam.
+*/
+#include <iostream>
+
+using namespace std;
+
+/* Fill in 4 functions below with proper inputs and return types */
+
+
+
+/* End of function insertion */
+
+// Don't touch main!
+int main(){
+  // Your goal is to fill in function above to make this file compile
+  // and match the expected output.
+  cout << "Multiply:" << endl;
+  cout << "1/2 = ";
+  multiply(1,1,1,2);
+  cout << endl << "1/8 = ";
+  multiply(1,4,1,2);
+  cout << endl << "2/9 = ";
+  multiply(-1,9,-2,1);
+  
+  cout << endl << endl << "Add:" << endl;
+  cout << "2/4 = ";
+  add(1,4,1,4);
+  cout << endl << "2/8 = ";
+  add(1,2,-1,4);
+  
+  cout << endl << endl << "Enough:" << endl;
+  cout << "3 = " << enough(9) << endl;
+  cout << "3 = " << enough(8) << endl;
+  cout << "5 = " << enough(17) << endl;
+  cout << "6 = " << enough(21) << endl;
+  cout << "0 = " << enough(-5) << endl;
+  cout << "19 = " << enough(4) + enough(23) + enough(77) << endl;
+  
+  cout << endl << "Sum from to:" << endl;
+  cout << "15 = " << sum_from_to(0,5) << endl;
+  cout << "15 = " << sum_from_to(5,0) << endl;
+  cout << "11 = " << sum_from_to(5,6) << endl;
+  cout << "81 = " << sum_from_to(0,3) + sum_from_to(10,15) << endl;
+  cout << "-15 = " << sum_from_to(4,-15) - sum_from_to(-14,4) << endl;
+  
+  return 0;
+}
